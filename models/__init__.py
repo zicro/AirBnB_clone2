78b819031c6777beb71b65b5e1ff5fe5ec1a7edd
Loubnaa1 <loubnamerzak4@gmail.com>
2023-11-23 00:54:46 +0100

    init_change

--- models/__init__.py
+++ models/__init__.py
@@ -6,13 +6,14 @@
 -> Otherwise, instantiates a file storage engine (FileStorage).
 """
 from os import getenv
-
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
 
 if getenv("HBNB_TYPE_STORAGE") == "db":
-    from models.engine.db_storage import DBStorage
+    
     storage = DBStorage()
 else:
-    from models.engine.file_storage import FileStorage
+    
     storage = FileStorage()
 storage.reload()
 
